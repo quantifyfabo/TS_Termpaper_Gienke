94a4e7f143def7be387ebb5f8d0873fab1bc715e
quantifyfabo <f.gienke@yahoo.de>
2025-01-18 23:21:06 +0100

    Updated the USA GT Setup, renamed variables and added search Terms: USA, Trump, Biden, Harris. All Merged into one Dataframe.

--- TS_02_GoogleTrends_Setup.R
+++ TS_02_GoogleTrends_Setup.R
@@ -4,25 +4,32 @@
 # Search Terms: "USA" , 
 
 
-# Loading Data
-GT_USA <- read.csv("C:/Datasets/tagesschau_folder/GT/GT_USA_1101.csv", header = FALSE)
-GT_USA <- GT_USA[-c(1,2), ] # Delete Row 1 and 2 !! Only run this line once!!
-colnames(GT_USA) <- c("dateGT", "USA_Score")
+# Data Preperation for the Google Trends Search Results for the Topic "USA"
+gt_usa_raw <- read.csv("C:/Datasets/tagesschau_folder/GT/GT_USA_1101.csv", header = FALSE)
+gt_trump_raw <- read.csv("C:/Datasets/tagesschau_folder/GT/GT_Trump_1101.csv", header = FALSE)
+gt_biden_raw <- read.csv("C:/Datasets/tagesschau_folder/GT/GT_Biden_1101.csv", header = FALSE)
+gt_harris_raw <- read.csv("C:/Datasets/tagesschau_folder/GT/GT_Harris_1101.csv", header = FALSE)
 
+gt_us <- Reduce(function(x, y) merge(x,y, by = "V1", all = T), list(gt_usa_raw, gt_trump_raw, gt_biden_raw, gt_harris_raw))
+colnames(gt_us) <- c("dateGT", "USA_ScoreGT", "Trump_ScoreGT", "Biden_ScoreGT", "Harris_ScoreGT")
+gt_us <- head(gt_us, -2)
 
 
 # Format the date and create $week variable
-GT_USA$dateGT <- as.Date(GT_USA$dateGT)
-GT_USA$dateGT <- GT_USA$dateGT + 3  # +3 days to cover right week (important to switch year to 2025 after week 52)
+gt_us$dateGT <- as.Date(gt_us$dateGT)
+gt_us$dateGT <- gt_us$dateGT + 3  # +3 days to cover right week (important to switch year to 2025 after week 52)
 
-GT_USA$week <- isoweek(GT_USA$dateGT)
+gt_us$week <- isoweek(gt_us$dateGT)
 
 # Make $week continue in 2025 to prevent duplicates
-GT_USA$week <- ifelse(
-  format(GT_USA$dateGT, "%Y") == "2025", 
-  as.numeric(GT_USA$week) + 52 ,  # Kalenderwochen um 52 erhöhen für 2025
-  as.numeric(GT_USA$week))        # Original-Woche für andere Jahre
+gt_us$week <- ifelse(
+  format(gt_us$dateGT, "%Y") == "2025", 
+  as.numeric(gt_us$week) + 52 ,  # Kalenderwochen um 52 erhöhen für 2025
+  as.numeric(gt_us$week))        # Original-Woche für andere Jahre
 
-GT_USA$USA_Score <- as.numeric(GT_USA$USA_Score) # make $USA_Score numeric
+#make all variables excep $dateGT to numeric 
+us_numvars <- c("USA_ScoreGT", "Trump_ScoreGT", "Biden_ScoreGT", "Harris_ScoreGT")
+gt_us[us_numvars] <- lapply(gt_us[us_numvars], as.numeric)
+
 
 
