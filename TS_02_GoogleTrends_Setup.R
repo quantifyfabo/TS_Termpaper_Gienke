38a5229fc84edc7c525ee8efa4dc0bfea25f45e6
quantifyfabo <f.gienke@yahoo.de>
2025-01-25 17:58:45 +0100

    Making Searched Topics for USA and Middle East working

--- TS_02_GoogleTrends_Setup.R
+++ TS_02_GoogleTrends_Setup.R
@@ -1,35 +1,81 @@
 # Before running the code, load all packages from "TS_03_DataPreperation"!
+# the code below uses data from Google Trends. Search terms were selected based on resulting topics from stm in "TS_04_TopicModels"
+# Topics: "USA" , "Middle East", "Ukraine"
 
-# The code below uses data from Google Trends. Search Terms were selected based on resulting topics from stm in "TS_04_TopicModels"
-# Search Terms: "USA" , 
 
+# US
 
-# Data Preperation for the Google Trends Search Results for the Topic "USA"
+# data preperation for the Google Trends data for the topic "USA"
+# load csv files of GT search terms "USA", "Trump", "Biden", "Harris" in the time frame 11.01.2024 to 11.01.2025
 gt_usa_raw <- read.csv("C:/Datasets/tagesschau_folder/GT/GT_USA_1101.csv", header = FALSE)
 gt_trump_raw <- read.csv("C:/Datasets/tagesschau_folder/GT/GT_Trump_1101.csv", header = FALSE)
 gt_biden_raw <- read.csv("C:/Datasets/tagesschau_folder/GT/GT_Biden_1101.csv", header = FALSE)
 gt_harris_raw <- read.csv("C:/Datasets/tagesschau_folder/GT/GT_Harris_1101.csv", header = FALSE)
 
+# Merge GT_raw csv files to one dataset based on week variable, rename variables, delete last 2 rows.
 gt_us <- Reduce(function(x, y) merge(x,y, by = "V1", all = T), list(gt_usa_raw, gt_trump_raw, gt_biden_raw, gt_harris_raw))
 colnames(gt_us) <- c("dateGT", "USA_ScoreGT", "Trump_ScoreGT", "Biden_ScoreGT", "Harris_ScoreGT")
 gt_us <- head(gt_us, -2)
 
-
 # Format the date and create $week variable
 gt_us$dateGT <- as.Date(gt_us$dateGT)
 gt_us$dateGT <- gt_us$dateGT + 3  # +3 days to cover right week (important to switch year to 2025 after week 52)
-
 gt_us$week <- isoweek(gt_us$dateGT)
 
 # Make $week continue in 2025 to prevent duplicates
 gt_us$week <- ifelse(
   format(gt_us$dateGT, "%Y") == "2025", 
-  as.numeric(gt_us$week) + 52 ,  # Kalenderwochen um 52 erhöhen für 2025
-  as.numeric(gt_us$week))        # Original-Woche für andere Jahre
+  as.numeric(gt_us$week) + 52 ,  # +52 weeks when year==2025
+  as.numeric(gt_us$week))        
 
-#make all variables excep $dateGT to numeric 
+#make all variables except $dateGT to numeric 
 us_numvars <- c("USA_ScoreGT", "Trump_ScoreGT", "Biden_ScoreGT", "Harris_ScoreGT")
 gt_us[us_numvars] <- lapply(gt_us[us_numvars], as.numeric)
+
+
+
+
+
+# Middle East
+
+# data preperation for the Google Trends data for the topic "Middle East"
+# load csv files of GT search terms "Israel", "Hamas", "Iran", "Gaza" in the time frame 11.01.2024 to 11.01.2025
+gt_israel_raw <- read.csv("C:/Datasets/tagesschau_folder/GT/GT_Israel_1101.csv", header = FALSE)
+gt_hamas_raw <- read.csv("C:/Datasets/tagesschau_folder/GT/GT_Hamas_1101.csv", header = FALSE)
+gt_iran_raw <- read.csv("C:/Datasets/tagesschau_folder/GT/GT_Iran_1101.csv", header = FALSE)
+gt_gaza_raw <- read.csv("C:/Datasets/tagesschau_folder/GT/GT_Gaza_1101.csv", header = FALSE)
+
+# Merge GT_raw csv files to one dataset based on week variable, rename variables, delete last 2 rows.
+gt_mideast <- Reduce(function(x, y) merge(x,y, by = "V1", all = T), list(gt_israel_raw, gt_hamas_raw, gt_iran_raw, gt_gaza_raw))
+colnames(gt_mideast) <- c("dateGT", "Israel_ScoreGT", "Hamas_ScoreGT", "Iran_ScoreGT", "Gaza_ScoreGT")
+gt_mideast <- head(gt_mideast, -2)
+
+# Format the date and create $week variable
+gt_mideast$dateGT <- as.Date(gt_mideast$dateGT)
+gt_mideast$dateGT <- gt_us$dateGT + 3  # +3 days to cover right week (important to switch year to 2025 after week 52)
+gt_mideast$week <- isoweek(gt_mideast$dateGT)
+
+# Make $week continue in 2025 to prevent duplicates
+gt_mideast$week <- ifelse(
+  format(gt_mideast$dateGT, "%Y") == "2025", 
+  as.numeric(gt_mideast$week) + 52 ,  # +52 weeks when year==2025
+  as.numeric(gt_mideast$week))        
+
+#make all variables except $dateGT to numeric 
+mideast_numvars <- c("dateGT", "Israel_ScoreGT", "Hamas_ScoreGT", "Iran_ScoreGT", "Gaza_ScoreGT")
+gt_mideast[mideast_numvars] <- lapply(gt_mideast[mideast_numvars], as.numeric)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
