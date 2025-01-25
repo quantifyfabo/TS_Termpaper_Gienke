a34965807412e66cec31b6bb22a37dc9508b3aa1
quantifyfabo <f.gienke@yahoo.de>
2025-01-25 18:03:49 +0100

    Make only merging code into this file. visualization is an external file now. Merging contains mideast working and us working

--- TS_05_Merging.R
+++ TS_05_Merging.R
@@ -10,33 +10,38 @@
 # Merging for "USA"
 # Filter for topic = USA
 # count the topic for each week
-# add result to the GT_USA Dataset
+# add result to the GT_[Topic] Dataset
 
+
+# Merge TS and gt_us
 TS_usa_counts <- TS_GT %>%
-  filter(dominant_topic == "USA") %>%        # Filter auf "USA" in der Variable dominant_topic
-  group_by(week) %>%                         # Gruppieren nach Woche
-  summarise(ts_count_usa = n()) %>%             # Anzahl der Fälle pro Woche zählen
-  ungroup()                                  # Gruppierung entfernen
+  filter(dominant_topic == "USA") %>%        # filter for "USA" as dominant_topic
+  group_by(week) %>%                         # goup by week
+  summarise(ts_count_usa = n()) %>%          # summarise by cases per week
+  ungroup()                                  # delete grouping
+
+# merging by dplyrs join
+gt_us <- gt_us %>%
+  left_join(TS_usa_counts, by = "week")         # merge based on $week (both in TS and gt)
+
+# Normalize ts_count_us based on 100% annual peak, add as new variable
+gt_us$ts_percentage_of_peak <- (gt_us$ts_count_usa / max(gt_us$ts_count_usa)) * 100
+
+
+
+# --
+
+# Merging the Mideast Topic from TS into gt_mideast
+TS_mideast_counts <- TS_GT %>%
+  filter(dominant_topic == "Middle_East") %>%        # filter for "USA" as dominant_topic
+  group_by(week) %>%                         # goup by week
+  summarise(ts_count_mideast = n()) %>%          # summarise by cases per week
+  ungroup()                                  # delete grouping
 
 # Neue Variable zu GT_USA hinzufügen
-gt_us <- gt_us %>%
-  left_join(TS_usa_counts, by = "week")         # Join nach Woche
+gt_mideast <- gt_mideast %>%
+  left_join(TS_mideast_counts, by = "week")         # merge based on $week (both in TS and gt)
 
+# Normalize ts_count_us based on 100% annual peak, add as new variable
+gt_mideast$ts_percentage_of_peak <- (gt_mideast$ts_count_mideast / max(gt_mideast$ts_count_mideast)) * 100
 
-
-# Visualize
-ggplot(gt_us, aes(x = week)) +
-  geom_line(aes(y = ts_count_usa, color = "Tagesschau"), size = 1.8) + 
-  geom_line(aes(y = USA_ScoreGT, color = "GT USA"), size = 0.6) +
-  geom_line(aes(y = Trump_ScoreGT, color = "GT Trump"), size = 0.6) +
-  geom_line(aes(y = Biden_ScoreGT, color = "GT Biden"), size = 0.6) +
-  geom_line(aes(y = Harris_ScoreGT, color = "GT Harris"), size = 0.6) +
-  scale_color_manual(values = c("Tagesschau" = "red", "GT USA" = "blue", "GT Trump" = "green", 
-                                "GT Biden" = "yellow", "GT Harris" = "black")) + 
-  labs(
-    title = "USA - Google Trends in % of the annual peak vs. number of tagesschau articles per week",
-    x = "Week",
-    y = "Value",
-    color = "Legend"
-  ) +
-  theme_minimal()
