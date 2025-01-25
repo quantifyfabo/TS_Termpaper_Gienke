0484a473313dc476d28b8cad8c8020fdd1668fd9
quantifyfabo <f.gienke@yahoo.de>
2025-01-25 18:31:29 +0100

    Added Ukraine Topic. Edit: Mideast date GT +3 was labeled with US not mideast <-- corrcted it, i hope it still works

--- TS_02_GoogleTrends_Setup.R
+++ TS_02_GoogleTrends_Setup.R
@@ -52,7 +52,7 @@ gt_mideast <- head(gt_mideast, -2)
 
 # Format the date and create $week variable
 gt_mideast$dateGT <- as.Date(gt_mideast$dateGT)
-gt_mideast$dateGT <- gt_us$dateGT + 3  # +3 days to cover right week (important to switch year to 2025 after week 52)
+gt_mideast$dateGT <- gt_mideast$dateGT + 3  # +3 days to cover right week (important to switch year to 2025 after week 52)
 gt_mideast$week <- isoweek(gt_mideast$dateGT)
 
 # Make $week continue in 2025 to prevent duplicates
@@ -67,6 +67,36 @@ gt_mideast[mideast_numvars] <- lapply(gt_mideast[mideast_numvars], as.numeric)
 
 
 
+
+
+
+# Ukraine
+# data preperation for the Google Trends data for the topic "Ukraine"
+# load csv files of GT search terms "Ukraine", "Russland", "Kiew", "Gaza" in the time frame 11.01.2024 to 11.01.2025
+gt_ukraine_raw <- read.csv("C:/Datasets/tagesschau_folder/GT/GT_Ukraine_1101.csv", header = FALSE)
+gt_russland_raw <- read.csv("C:/Datasets/tagesschau_folder/GT/GT_Russland_1101.csv", header = FALSE)
+gt_kiew_raw <- read.csv("C:/Datasets/tagesschau_folder/GT/GT_Kiew_1101.csv", header = FALSE)
+gt_selenskyj_raw <- read.csv("C:/Datasets/tagesschau_folder/GT/GT_Selenskyj_1101.csv", header = FALSE)
+
+# Merge GT_raw csv files to one dataset based on week variable, rename variables, delete last 2 rows.
+gt_ukraine <- Reduce(function(x, y) merge(x,y, by = "V1", all = T), list(gt_ukraine_raw, gt_russland_raw, gt_kiew_raw, gt_selenskyj_raw))
+colnames(gt_ukraine) <- c("dateGT", "Ukraine_ScoreGT", "Russland_ScoreGT", "Kiew_ScoreGT", "Selenskyj_ScoreGT")
+gt_ukraine <- head(gt_ukraine, -2)
+
+# Format the date and create $week variable
+gt_ukraine$dateGT <- as.Date(gt_ukraine$dateGT)
+gt_ukraine$dateGT <- gt_ukraine$dateGT + 3  # +3 days to cover right week (important to switch year to 2025 after week 52)
+gt_ukraine$week <- isoweek(gt_ukraine$dateGT)
+
+# Make $week continue in 2025 to prevent duplicates
+gt_ukraine$week <- ifelse(
+  format(gt_ukraine$dateGT, "%Y") == "2025", 
+  as.numeric(gt_ukraine$week) + 52 ,  # +52 weeks when year==2025
+  as.numeric(gt_ukraine$week))        
+
+#make all variables except $dateGT to numeric 
+ukraine_numvars <- c("dateGT", "Ukraine_ScoreGT", "Russland_ScoreGT", "Kiew_ScoreGT", "Selenskyj_ScoreGT")
+gt_ukraine[ukraine_numvars] <- lapply(gt_ukraine[ukraine_numvars], as.numeric)
 
 
 
